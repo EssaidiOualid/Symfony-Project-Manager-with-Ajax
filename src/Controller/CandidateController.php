70f26d954c718ae4a3a2817dc9f6996b6224f328
oumaima <oumaimaena48@gmail.com>
2024-02-08 15:36:30 +0100

    from oumaima

--- src/Controller/CandidateController.php
+++ src/Controller/CandidateController.php
@@ -3,15 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Candidate;
+use App\Repository\PostRepository;
+use App\Repository\TypeRepository;
 use App\Repository\CandidateRepository;
 use App\Repository\CategorieRepository;
-use App\Repository\PostRepository;
+use App\Repository\SessionRepository;
 use App\Repository\SpecialiteRepository;
-use App\Repository\TypeRepository;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CandidateController extends AbstractController
@@ -21,6 +23,7 @@ class CandidateController extends AbstractController
     private $categorieRepository;
     private $specialiteRepository;
     private $clientRepository;
+    private $sessionRepository;
     private $manager;
     public function __construct(
         EntityManagerInterface $manger,
@@ -28,8 +31,10 @@ class CandidateController extends AbstractController
         SpecialiteRepository $specialiteRepository,
         CategorieRepository $categorieRepository,
         PostRepository $postRepository,
-        TypeRepository $typeRepository
+        TypeRepository $typeRepository,
+        SessionRepository $sessionRepository
     ) {
+        $this->sessionRepository = $sessionRepository;
         $this->TypeRepository = $typeRepository;
         $this->PostRepository = $postRepository;
         $this->categorieRepository = $categorieRepository;
@@ -63,39 +68,42 @@ class CandidateController extends AbstractController
     }
 
     #[Route('/posts/get', name: 'get_posts', methods: 'get')]
-    public function getposts() : JsonResponse{
+    public function getposts(): JsonResponse
+    {
         $posts = $this->PostRepository->findBy(['Session' => $this->sessionRepository->findOneBy(['active' => true])]);;
         $posts_data = [];
         foreach ($posts as $post)
-        array_push($posts_data, 
-    ['id' => $post->getSpecialite()->getId(),
-    //'session' => $post->getSession()->getSession(),
-    'specialite' => $post->getSpecialite()->getIntitule(),
-    //'categorie' => $post->getCategorie()->getIntitule(),
-    //'nbr_post' => $post->getNbrPost(),
-    //'nbr_reste' => $post->getNbrReste()  
-    'nbr_reste' => [$post->getCategorie()->getIntitule() => $post->getNbrReste()]
-    ]
-    ); 
-    $newArray = [];
+            array_push(
+                $posts_data,
+                [
+                    'id' => $post->getSpecialite()->getId(),
+                    //'session' => $post->getSession()->getSession(),
+                    'specialite' => $post->getSpecialite()->getIntitule(),
+                    //'categorie' => $post->getCategorie()->getIntitule(),
+                    //'nbr_post' => $post->getNbrPost(),
+                    //'nbr_reste' => $post->getNbrReste()  
+                    'nbr_reste' => [$post->getCategorie()->getIntitule() => $post->getNbrReste()]
+                ]
+            );
+        $newArray = [];
 
-    foreach ($posts_data as $item) {
-        $id = $item['id'];
-        $specialite = $item['specialite'];
-    
-        if (!isset($newArray[$id][$specialite])) {
-            $newArray[$id][$specialite] = [
-                'id' => $id,
-                'specialite' => $specialite,
-                'nbr_reste' => []
-            ];
+        foreach ($posts_data as $item) {
+            $id = $item['id'];
+            $specialite = $item['specialite'];
+
+            if (!isset($newArray[$id][$specialite])) {
+                $newArray[$id][$specialite] = [
+                    'id' => $id,
+                    'specialite' => $specialite,
+                    'nbr_reste' => []
+                ];
+            }
+
+            $newArray[$id][$specialite]['nbr_reste'] = array_merge($newArray[$id][$specialite]['nbr_reste'], $item['nbr_reste']);
         }
-    
-        $newArray[$id][$specialite]['nbr_reste'] = array_merge($newArray[$id][$specialite]['nbr_reste'], $item['nbr_reste']);
-    }
-    
-    $newArray = array_map('array_values', $newArray);
-    $newArray = array_merge(...$newArray);        
+
+        $newArray = array_map('array_values', $newArray);
+        $newArray = array_merge(...$newArray);
         return new JsonResponse($newArray);
     }
 }
